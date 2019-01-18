7fdb4819326d3e4d12df195e64ec86ceb86f522a
Bodo Schulz <bodo@boone-schulz.de>
2019-01-18 06:57:53 +0100

    fix automatic core user creation

--- Makefile
+++ Makefile
@@ -1,11 +1,11 @@
-export GIT_SHA1          := $(shell git rev-parse --short HEAD)
-export DOCKER_IMAGE_NAME := quassel-core
-export DOCKER_NAME_SPACE := ${USER}
-export DOCKER_VERSION    ?= latest
-export BUILD_DATE        := $(shell date +%Y-%m-%d)
-export BUILD_VERSION     := $(shell date +%y%m)
-export BUILD_TYPE        ?= stable
-export QUASSELWEB_VERSION ?= 2.2.8
+export GIT_SHA1            := $(shell git rev-parse --short HEAD)
+export DOCKER_IMAGE_NAME   := quassel-core
+export DOCKER_NAME_SPACE   := ${USER}
+export DOCKER_VERSION      ?= latest
+export BUILD_DATE          := $(shell date +%Y-%m-%d)
+export BUILD_VERSION       := $(shell date +%y%m)
+export BUILD_TYPE          ?= stable
+export QUASSELCORE_VERSION ?= 0.13.0
 
 .PHONY: build shell run exec start stop clean compose-file
 
--- rootfs/init/run.sh
+++ rootfs/init/run.sh
@@ -1,53 +1,123 @@
 #!/bin/sh
 
 LISTEN=${LISTEN:-"0.0.0.0"}
-LOGLEVEL=${LOGLEVEL:-"Debug"}
+LOGLEVEL=${LOGLEVEL:-"Info"}
 PORT=${PORT:="4242"}
-
 CONFIG_DIR=/var/lib/quassel
 
-# generate key
-if [ ! -f ${CONFIG_DIR}/quasselCert.pem ]
-then
-  openssl req \
-    -x509 \
-    -nodes \
-    -days 365 \
-    -newkey rsa:4096 \
-    -keyout ${CONFIG_DIR}/quasselCert.pem \
-    -out ${CONFIG_DIR}/quasselCert.pem \
-    -subj "/CN=Quassel-core"
+QUASSELCORE_USER=${QUASSELCORE_USER:-quasselcore}
+QUASSELCORE_PASSWORD=${QUASSELCORE_PASSWORD:-quasselcore}
 
+. /init/output.sh
 
-#  openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout quasselCert.key -out quasselCert.crt && cat quasselCert.{key,crt} > quasselCert.pem
-fi
+# -------------------------------------------------------------------------------------------------
 
-if [ ! ${CONFIG_DIR}/quassel-storage.sqlite ]
-then
+finish() {
+  rv=$?
+  [ ${rv} -gt 0 ] && echo -e "\033[38;5;202m\033[1mexit with signal '${rv}'\033[0m"
+  exit $rv
+}
+
+trap finish SIGINT SIGTERM INT TERM EXIT
+
+# -------------------------------------------------------------------------------------------------
+
+watch_and_kill() {
+  sleep 5s
+  killall -9 quasselcore
+}
+
+create_vertificate() {
+
+  # generate key
+  if [ ! -f ${CONFIG_DIR}/quasselCert.pem ]
+  then
+    log_info "create certificate"
+
+  #  openssl req \
+  #    -x509 \
+  #    -nodes \
+  #    -days 365 \
+  #    -newkey rsa:4096 \
+  #    -keyout ${CONFIG_DIR}/quasselCert.pem \
+  #    -out ${CONFIG_DIR}/quasselCert.pem \
+  #    -subj "/CN=Quassel-core"
+
+    openssl req \
+      -x509 \
+      -nodes \
+      -days 365 \
+      -newkey rsa:4096 \
+      -keyout ${CONFIG_DIR}/quasselCert.key \
+      -out ${CONFIG_DIR}/quasselCert.crt \
+      -subj "/CN=Quassel-core" && \
+    cat \
+      ${CONFIG_DIR}/quasselCert.key \
+      ${CONFIG_DIR}/quasselCert.crt \
+      > ${CONFIG_DIR}/quasselCert.pem
+  fi
+}
+
+create_database() {
+
+  if [ ! -f  ${CONFIG_DIR}/quassel-storage.sqlite ]
+  then
+    log_info "create backend database"
+
+    watch_and_kill &
+
+    quasselcore \
+      --configdir=${CONFIG_DIR} \
+      --loglevel=Info \
+      --select-backend=SQLite > /dev/null
+  fi
+}
+
+start_quasselcore() {
+
+  # permissions
+  chown -R quassel:quassel \
+    ${CONFIG_DIR}
+
+  command_args="
+    --configdir=${CONFIG_DIR}
+    --require-ssl
+    --listen=${LISTEN}
+    --loglevel=${LOGLEVEL}
+    --port=${PORT}"
+
+  log_info "start quasselcore"
+
   quasselcore \
-    --configdir=${CONFIG_DIR}  \
-    --select-backend=SQLite &
+    ${command_args}
+}
 
-  killall quasselcore
-else
-  /usr/bin/manageusers.py list
+add_quasselcore_user() {
 
-fi
+  if [ $(python2 /usr/bin/manageusers.py list | wc -l) -eq 1 ]
+  then
+    log_info "add core user ${QUASSELCORE_USER}"
+
+    python2 \
+      /usr/bin/manageusers.py add \
+      ${QUASSELCORE_USER} \
+      ${QUASSELCORE_PASSWORD} > /dev/null
+  fi
+}
 
 
-# permissions
-#chown -R abc:abc \
-#  ${CONFIG_DIR}
+run() {
 
-command_args="
-  --configdir=${CONFIG_DIR}
-  --require-ssl
-  --select-backend=SQLite
-  --listen=${LISTEN}
-  --loglevel=${LOGLEVEL}
-  --port=${PORT}
-  --logfile=/dev/fd/1"
+  create_vertificate
 
-quasselcore \
-  ${command_args}
+  create_database
 
+  add_quasselcore_user
+
+  start_quasselcore
+
+  #python2 /usr/bin/manageusers.py list
+}
+
+
+run
